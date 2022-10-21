156fecdd924942cda983b9a02e4e29a5ba59bc16
kahn <644812264@qq.com>
2022-10-21 15:01:57 +0800

    更新

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })
-  );
   await app.listen(3000);
 }
 
